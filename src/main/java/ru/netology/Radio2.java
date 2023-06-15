52154759a78daf418048be3c7861dadb820f7f03
Ebg79 <129846734+Ebg79@users.noreply.github.com>
2023-06-15 20:25:04 +0300

    Update Lombok

--- src/main/java/ru/netology/Radio2.java
+++ src/main/java/ru/netology/Radio2.java
@@ -1,19 +1,69 @@
 package ru.netology;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-
 public class Radio2 {
-     private int currentStation;
+    private int currentStation;
     private int minStation = 0;
     private int maxStation = 9;
     private int quantityStation = 10;
     private int currentVolume;
     private int minVolume = 0;
     private int maxVolume = 100;
+
+    public Radio2() {
+    }
+
+    public Radio2(int quantityStation) {
+        this.quantityStation = quantityStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation || currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (maxStation <= currentStation) {
+            setCurrentStation(minStation);
+        } else {
+            setCurrentStation(currentStation + 1);
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation <= minStation) {
+            setCurrentStation(maxStation);
+        } else {
+            setCurrentStation(currentStation - 1);
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume++;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume--;
+        }
+    }
 }
